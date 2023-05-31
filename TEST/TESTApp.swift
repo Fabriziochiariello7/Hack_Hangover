47e883f004abdeef10e05fa81643efcc789b5dba
Fabriziochiariello7 <113529227+Fabriziochiariello7@users.noreply.github.com>
2023-05-31 15:08:19 +0200

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by Fabrizio Chiariello on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
