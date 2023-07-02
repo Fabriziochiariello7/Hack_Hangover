de4f97911df3cf58f599942533d6426e22521342
Martons00 <raffaelemartone34@gmail.com>
2023-07-02 11:28:35 +0200

    AnimationBAll

--- TEST/AnimationCircle.swift
+++ TEST/AnimationCircle.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct AnimationCircle: View {
+    @State var gesture : String = ""
+    @State var andata = true
+    @State var scaleFlag : Double = 0
+    @State var doubleDown : Bool = false
+    @State var upOutside : Bool = false
+    @State var pos = CGSize(width: 0.0, height: 0.0)
+    let timer = Timer
+        .publish(every: 2, on: .main, in: .common)
+        .autoconnect()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Circle()
+            .scaleEffect(0.8)
+            .foregroundColor(.blue)
+            .offset(x: pos.width, y: pos.height)
+            .animation(.linear(duration: 2.0), value: pos)
+        
+            .onReceive(timer) { time in
+                if andata{
+                    pos.width =  Double.random(in: -20.0..<20.0)
+                    pos.height = Double.random(in: -20.0..<20.0)
+                        andata = false
+                }else{
+                    pos.width = Double.random(in: -20.0..<20.0)
+                    pos.height = Double.random(in: -20.0..<20.0)
+                        andata = true
+                }
+            }
     }
 }
+
+
+
 
 struct AnimationCircle_Previews: PreviewProvider {
     static var previews: some View {
